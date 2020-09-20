70492db9636baf41f76cfbe9b1ac8ea42bcac355
marcsello <punkosdmarcell@rocketmail.com>
2020-09-20 22:06:42 +0200

    added completion for first commands

--- kubecli/interpreter.py
+++ kubecli/interpreter.py
@@ -2,36 +2,52 @@
 from typing import List, Optional
 from cmd import Cmd
 from kubectl import Kubectl
+import readline
 
 
 class KubeCliInterpreter(Cmd):
+    doc_header = "Documented internal commands (type /help <topic>):"
+    misc_header = "Miscellaneous help topics:"
+    undoc_header = "Undocumented internal commands:"
+
+    kubectl_first_commands = [  # Copied from kubectl help
+        'create', 'expose', 'run', 'set', 'explain', 'get', 'edit', 'delete', 'rollout', 'scale', 'autoscale',
+        'certificate', 'cluster-info', 'top', 'cordon', 'uncordon', 'drain', 'taint', 'describe', 'logs', 'attach',
+        'exec', 'port-forward', 'proxy', 'cp', 'auth', 'diff', 'apply', 'patch', 'replace', 'wait', 'convert',
+        'kustomize', 'label', 'annotate', 'completion', 'alpha', 'api-resources', 'api-versions', 'config', 'plugin',
+        'version',
+    ]
 
     def __init__(self, kubectl: Kubectl):
         super().__init__()
         self._kubectl = kubectl
         self._last_command_failed = False
+        self.old_delims = None
+
+    def preloop(self) -> None:
+        self.old_delims = readline.get_completer_delims()
+        readline.set_completer_delims(self.old_delims.replace('-', '').replace('/', ''))
+
+    def postloop(self) -> None:
+        readline.set_completer_delims(self.old_delims)
 
     def onecmd(self, line: str) -> bool:
         self._last_command_failed = False
-        if line.startswith('/'):
-            return self._process_internal_command(line)
-        else:
+
+        if line == 'EOF':
+            return True
+        elif line.startswith('/'):
             return super().onecmd(line)
-
-    def _process_internal_command(self, line: str) -> bool:
-        cmd, arg, line = self.parseline(line[1:])
-        if cmd:
-            try:
-                func = getattr(self, 'internal_' + cmd)
-            except AttributeError:
-                print(f"*** Unknown internal command: {cmd}")
-                return False
-            return func(arg)
         else:
-            print("*** Invalid syntax: internal command expected")
-            return False
+            if line:
+                self.run_kubectl_command(line)
+            else:
+                self.emptyline()
 
-    def internal_setns(self, args):
+    def parseline(self, line):
+        return super().parseline(line[1:])  # onecmd ensures that there is a "/"
+
+    def do_setns(self, args):
         'Switch between namesapces'
         if not args:
             print("*** Invalid syntax: a single namespace expected")
@@ -40,44 +56,7 @@ class KubeCliInterpreter(Cmd):
             if not success:
                 self._last_command_failed = True
 
-    def internal_help(self, arg):
-        'List available internal commands with "/help" or detailed help with "/help cmd".'
-        if arg:
-            # XXX check arg syntax
-            try:
-                doc = getattr(self, 'internal_' + arg).__doc__
-                if doc:
-                    self.stdout.write("%s\n" % str(doc))
-                    return
-            except AttributeError:
-                pass
-            self.stdout.write("%s\n" % str(self.nohelp % (arg,)))
-            return
-        else:
-            names = [a for a in self.get_names() if a.startswith('internal_')]
-            cmds_doc = []
-            cmds_undoc = []
-            for name in names:
-                cmd = name[9:]
-                if getattr(self, name).__doc__:
-                    cmds_doc.append(cmd)
-                else:
-                    cmds_undoc.append(cmd)
-            self.stdout.write("%s\n" % str(self.doc_leader))
-            self.print_topics("Documented internal commands (type /? <topic>):", cmds_doc, 15, 80)
-            self.print_topics("Undocumented internal commands:", cmds_undoc, 15, 80)
-
-    def complete_internal_names(self, text, *ignored):
-        internaltext = 'internal_' + text
-        return ['/' + a[9:] for a in self.get_names() if a.startswith(internaltext)]
-
-    def do_help(self, arg: str):
-        if arg:
-            self.default('help')
-        else:
-            self.default('help ' + arg)
-
-    def default(self, line: str) -> bool:
+    def run_kubectl_command(self, line: str) -> bool:
         parts = line.split(' ')
         ret = self._kubectl.run(parts)
         if ret != 0:
@@ -86,6 +65,28 @@ class KubeCliInterpreter(Cmd):
     def emptyline(self):
         # Do nothing on an empty line
         pass
+
+    def complete(self, text, state):
+        # The first iteration should collect possible completions
+        if state == 0:
+
+            if text == '':
+                self.completion_matches = ["/" + n + " " for n in self.completenames('')]
+                self.completion_matches += self.kubectl_first_commands
+            elif text.startswith('/'):
+                self.completion_matches = ["/" + n + " " for n in self.completenames(text[1:])]
+            elif text.count(' ') == 0:  # not starting with slash
+                self.completion_matches = [n + " " for n in self.kubectl_first_commands if n.startswith(text)]
+            else:  # not first command
+                self.completion_matches = []
+
+        try:
+            return self.completion_matches[state]
+        except IndexError:
+            return None
+
+    def do_exit(self, args):
+        return True
 
     @property
     def prompt(self) -> str:
